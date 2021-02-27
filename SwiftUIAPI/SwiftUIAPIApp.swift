a4da94b9a67431e9ee293f1c88cd04eedcbf77e6
joferava <jfelipe221095@gmail.com>
2021-02-27 17:47:16 -0600

    Initial Commit

---
+++ SwiftUIAPI/SwiftUIAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAPIApp.swift
+//  SwiftUIAPI
+//
+//  Created by Felipe Ramirez Vargas on 27/2/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

8318213ff8ef133d719f22f1c5d014fa14defba8
joferava <jfelipe221095@gmail.com>
2021-02-27 18:13:11 -0600

    First Commit

---
+++ SwiftUIAPI/CatFact.swift
@@ -0,0 +1,13 @@
+//
+//  CatFact.swift
+//  SwiftUIAPI
+//
+//  Created by Felipe Ramirez Vargas on 27/2/21.
+//
+
+import Foundation
+
+struct CatFact: Codable {
+    let _id: String
+    let text: String
+}
--- SwiftUIAPI/ContentView.swift
+++ SwiftUIAPI/ContentView.swift
@@ -8,10 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var results = [CatFact]()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List(results, id: \._id){ item in
+            VStack(alignment: .leading){
+                Text(item.text)
+            }
+        }.onAppear(perform: loadData)
     }
+    
+    func loadData(){
+        guard let url = URL(string: "https://cat-fact.herokuapp.com/facts") else {
+            print("Your API endpoint is invalid")
+            return
+        }
+        let request = URLRequest(url: url)
+        
+        URLSession.shared.dataTask(with: request){data, response, error in
+            if let data = data {
+                if let response = try? JSONDecoder().decode([CatFact].self, from: data){
+                    DispatchQueue.main.async {
+                        self.results = response
+                    }
+                    return
+                }
+            }
+        }.resume()
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
